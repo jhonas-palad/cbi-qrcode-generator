6d2e4b54b949aeb0990e3edcd555f56f7b6685ee
Jhonas Palad <jhonasemmanuel@gmail.com>
2023-03-22 10:14:05 +0800

    file types select implemented

--- src/App.js
+++ src/App.js
@@ -1,90 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState, useRef, useCallback} from 'react';
-import QRCode from "react-qr-code";
-import { QRCodeCanvas, QRCodeSVG } from 'qrcode.react';
+import React, {useState, useReducer} from 'react';
+import { downloadFile } from './utils';
+import { QRCodeCanvas } from 'qrcode.react';
+import { v4 as uuidv4 } from 'uuid';
 
-//Convert SVG to canvas -> img and download 
+import './index.css';
 
-const _download = (refElement) => {
-    const svgXML = new XMLSerializer().serializeToString(refElement);
-
-}
-
-const copyToCanvas = (refElement, scale = 10)=>{
-
-    const qrCanvas = document.getElementById('generated_qrcode');
-
-    return new Promise(resolve => {
-      resolve(qrCanvas);
-    });
-}; 
-
-
-const downloadFile = async (refElement, filename) => {
-  return copyToCanvas(refElement).then(
-    canvas => {
-      // const urlDownload = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-      const urlDownload = canvas.toDataURL('image/png')
-      const anchorTag = document.createElement("a");
-      anchorTag.href = urlDownload;
-      anchorTag.download=filename;
-      document.body.appendChild(anchorTag);
-      anchorTag.click();
-      document.body.removeChild(anchorTag);
-    }
-  )
-}
+//TODO:
+//Add css
 
 function App() {
-  const [urlInput, setUrlInput] = useState("");
-  const [urlToConvert, setUrlToConvert] = useState("");
-  const qrRef = useRef(null);
+    const [urlInput, setUrlInput] = useState("");
+    const [urlToConvert, setUrlToConvert] = useState("");
+    const [fileType, setFileType] = useState("png");
+    const [showDownload, setShowDownload] = useState(false);
+    const downloadQRCode = () => {
+        const generatedQRCodeRef = document.getElementById("generated_qrcode");
+        downloadFile(generatedQRCodeRef, uuidv4(), fileType);
+    }
 
-
-  const downloadSVG = () => {
-    const generated_svg = document.getElementById("generated_qrcode");
-    downloadFile(qrRef.current, 'Example.png');
-  }
   return (
-    <div className="App center">
-      <h1></h1>
-      <form style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems:'center',
-        justifyContent: 'center',
-        gap: 10
-      }}>
-          <label>
-              Enter a URL
-          </label>
-          <input
-            onChange={(e)=>setUrlInput(e.target.value)}
-            value={urlInput}
-          />
-        
-        <button onClick={()=> {
-           setUrlToConvert(urlInput)
-        }} type="button">Generate</button>
-      </form>
-      <div style={{
-        padding:8
-      }}>
-        
-      </div>
-      {/* <div style={{marginBotton: 5}}>
-        <QRCode size={128 * 2} value={urlToConvert}/>
-      </div> */}
-      <div>
-        <QRCodeCanvas style={{padding:10, background:"red"}} id="generated_qrcode" size={128 * 2} value={urlToConvert}/>
-      </div>
-      <div>
-        <button onClick={downloadSVG}>
-          Download
-        </button>
-        {/* <img src={require('./logo.png')} onLoad={()=>console.log("HELLO")}/> */}
-      </div>
+    <div className="center">
+        {/* <div className="flex-row flex flex-center logo-name-row">
+            <img src={require('./logo.png')} height="100" width="100" alt="cbi-logo" />
+            <h3>
+                CENTER FOR BUSINESS & INNOVATION
+            </h3>
+        </div> */}
+        <h2 style={{textAlign: 'center'}}>
+            QR CODE <br/>
+            GENERATOR
+        </h2>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems:'center',
+            justifyContent: 'center',
+            gap: 10,
+            border: '1px solid'
+        }}>
+            <div className='form-group'>
+                <label>Enter your URL</label>
+                <input
+                    onChange={(e)=>setUrlInput(e.target.value)}
+                    value={urlInput}
+                    type='url'
+                />
+            </div>
+            <div className='qr-out'>
+                {
+                    urlToConvert ? ( 
+                    <QRCodeCanvas id="generated_qrcode" size={128 * 2} value={urlToConvert}/>) : (
+                        <></>
+                    )
+                }   
+            </div>
+            <div className="flex-col flex btn-wrapper">
+            {
+                !showDownload ? (
+                    <button onClick={()=> {
+                        setUrlToConvert(urlInput);
+                        setShowDownload(true);
+                        }} type="button">
+                            Generate
+                    </button>
+                ) : (
+                    <>
+                        <div>
+
+                            <select onChange={(e) => setFileType(e.target.value)} name="selectedType">
+                                <option value="png">PNG</option>
+                                <option value="jpg">JPEG</option>
+                                <option value="svg">SVG</option>
+                            </select>
+                        </div>
+                        <button onClick={downloadQRCode}>
+                            Download
+                        </button>
+                        <a href={window.location.href}>Generate another QR code</a>
+                    </>
+                )
+            }
+            </div>
+
+            
+        </div>
     </div>
   );
 }
